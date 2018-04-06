75eea3e4969d016afb37d8c08db5fbdb2066300b
Zane Rockenbaugh <zane@liquid-labs.com>
2018-04-06 09:40:04 -0500

    fix impl errors

--- rest/json.go
+++ rest/json.go
@@ -10,14 +10,17 @@ type standardResponse struct {
   Data interface{} `json:"data"`
 }
 
-func PackageResponse(w http.ResponseWriter, d interface{}) (*standardResponse, error) {
-  resp := standardResponse{data: d}
+func PackageResponse(w http.ResponseWriter, d interface{}) (error) {
+  resp := standardResponse{Data: d}
 
-  if respBody, err := json.Marshal(resp); err != nil {
+  var respBody []byte
+  var err error
+  if respBody, err = json.Marshal(resp); err != nil {
     // TODO: hide error and give reference number
     http.Error(w, fmt.Sprintf("Could not format response: %v", err), 500)
-    return _, err
+    return err
   }
+  w.Write(respBody)
 
-  return respBody, _
+  return nil
 }
