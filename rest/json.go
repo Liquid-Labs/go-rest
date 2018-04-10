6c3cd8a0efe80878c00b359f1af54622417a2880
Zane Rockenbaugh <zane@liquid-labs.com>
2018-04-10 10:41:57 -0500

    standard post response

--- rest/json.go
+++ rest/json.go
@@ -49,3 +49,13 @@ func StandardServerError(w http.ResponseWriter, message string) {
   }
   http.Error(w, message, 500)
 }
+
+func HandlePost(w http.ResponseWriter, r *http.Request, d interface{}, dDesc string) {
+  decoder := json.NewDecoder(r.Body)
+
+  if err := decoder.Decode(d); err != nil {
+    StandardServerError(w, fmt.Sprintf("Could not decode %s payload: %v", dDesc, err))
+    return
+  }
+  defer r.Body.Close()
+}
